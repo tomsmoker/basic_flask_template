8f2857709e130727fd749a64c2b9644a287bde6f
tomsmoker <tomsmoker-uwa@gmail.com>
2022-04-29 10:05:09 +0800

    Working base version with css

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,28 @@
+from flask import render_template, url_for
+
 from app import app 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello, world."
+    
+    user = {
+        'username': "tom"
+    }
+    
+    quotes_i_like = [
+        {
+            'author': {'username': "mum"},
+            'text': "he means well"
+        },
+        {
+            'author': {'username': "kawhi leonard"},
+            'text': "i'm a fun guy"
+        },
+        {
+            'author': {'username': "bette midler"},
+            'text': "the worst part of success is to try to find someone who is happy for you."
+        } 
+    ]
+    
+    return render_template('index.html', title="toms page", user=user, quotes=quotes_i_like)
